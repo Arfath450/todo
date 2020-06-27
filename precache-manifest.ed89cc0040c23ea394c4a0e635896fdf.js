aca7da190fe1908fbd161b23750d4610d287e830
Arfath450 <46299355+Arfath450@users.noreply.github.com>
2020-06-27 10:48:14 +0530

    Updates

---
+++ precache-manifest.ed89cc0040c23ea394c4a0e635896fdf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c948a5d9482cd9a3683939713aa0b7c0",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "780b9ef667ca2424b369",
+    "url": "/todo/static/css/main.983a06e8.chunk.css"
+  },
+  {
+    "revision": "982d0f109e28a29811e8",
+    "url": "/todo/static/js/2.b492a714.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo/static/js/2.b492a714.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "780b9ef667ca2424b369",
+    "url": "/todo/static/js/main.e68ae55e.chunk.js"
+  },
+  {
+    "revision": "da7c3f7f7e5963fe74b8",
+    "url": "/todo/static/js/runtime-main.74315f96.js"
+  }
+]);
